c41f94b50d69d63b2b3a82862109599fb390bca5
Daniela Segura <daniela.segura@orsan.com>
2026-07-16 09:16:47 -0600

    Se añade vista nueva

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import StationsView from '../views/StationsView.vue'
 import FiscalView from '../views/FiscalView.vue'
 import QueEsUltraGasView from '../views/QueEsUltraGasView.vue'
+import GraciasView from '../views/GraciasView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,7 @@ const router = createRouter({
     { path: '/cobertura', name: 'cobertura', component: StationsView },
     { path: '/fiscal', name: 'fiscal', component: FiscalView },
     { path: '/que-es-ultragas', name: 'que-es-ultragas', component: QueEsUltraGasView },
+    { path: '/gracias', name: 'gracias', component: GraciasView },
   ],
   scrollBehavior() {
     return { top: 0 }
